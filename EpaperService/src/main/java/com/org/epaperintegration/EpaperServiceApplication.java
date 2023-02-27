37d048da67177fe90d0072f5564b67833a7e9f77
Manish Luste <59191348+manish24-tech@users.noreply.github.com>
2023-02-27 16:19:13 +0530

    delete the project

--- EpaperService/src/main/java/com/org/epaperintegration/EpaperServiceApplication.java
+++
@@ -1,9 +0,0 @@
-package com.org.epaperintegration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class EpaperServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EpaperServiceApplication.class, args);
-	}
-}
